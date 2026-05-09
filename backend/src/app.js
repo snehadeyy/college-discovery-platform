c23de4885fac23176da14d8166e97b53c959df9c
Sneha Dey <snehadeytechno@gmail.com>
2026-05-09 19:13:05 +0530

    changes added

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,10 +3,24 @@ import cors from "cors";
 
 const app = express()
 
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  process.env.PROD_CLIENT_URL
+];
+
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173'
-}))
+  origin: function (origin, callback) {
+    // allow requests with no origin (like Postman)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  }
+}));
 
 import { collegeRouter } from "./routes/college.routes.js";
 
